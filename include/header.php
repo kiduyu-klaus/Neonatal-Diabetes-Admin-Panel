bfc57b4ed15ea6a8ceabccdcb65997076d0775c9
kiduyu-klaus <muigakiduyu1995@gmail.com>
2020-07-23 08:50:23 -0700

    dashboard update

--- include/header.php
+++ include/header.php
@@ -144,7 +144,7 @@ include('include/config.php');
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
+    <a href="" class="brand-link">
       <img src="dist/img/Logo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
            style="opacity: .8">
       <span class="brand-text font-weight-light">Neonatal Sys Admin</span>
